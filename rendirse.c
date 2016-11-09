ee61178160bb1fb8d8051dba6b1805b80bcdb3c3
OscarSTK <starky78@outlook.com>
2016-11-08 18:02:08 -0600

    Update rendirse.c
    Complete la función rendirse

--- rendirse.c
+++ rendirse.c
@@ -1,4 +1,9 @@
-int rendirse ( tablero[8][8])
+/*Para esta función no es necesario incluir el tablero, solo una forma de llamarlo*/
+/*En la linea de comado podemos poner que si detecta un "GIVEUP" mande a llamar esta función rendirse.c*/
+#include <stdi.o>
+
+void rendirse () /*Que sea Void, al fin y al cabo no hay que regresar nada*/
 {
-   return 0;
+   printf("Gracias por jugar y suerte para la próxima");
+   exit(-1); /*Aqui alguna manera d finalizar el programa, la verdad no me acuerdo creo es asi, diganme si me equivoco.*/
 }
